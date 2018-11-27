bc500afbd17d9ffca0d6b74e0545bdb5c06f7ef0
megan muirhead <MeganM@natr-temps.com>
2018-11-26 17:14:50 -0700

    have the inputs posting to accordian

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  storedPosts = [];
   title = 'schoolCapStone';
+
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
