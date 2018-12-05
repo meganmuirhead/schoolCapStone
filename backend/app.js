8ac0ee3919dc450199ff0e077222b4271514024d
megan muirhead <MeganM@natr-temps.com>
2018-12-05 13:53:06 -0700

    posting to my database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
+
+mongoose.connect('mongodb+srv://megaboty:s02FazbMz5V9AERVpo@finalschoolproject-tjz1z.mongodb.net/node-angular?retryWrites=true')
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +29,12 @@ app.use((req, res, next) => {
 
 // only triggered for incoming post requests
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  // automatically creates the right query to insert the right data into the db
+  post.save();
   console.log(post);
   res.status(201).json({
     message: "Post added successfully!"
@@ -27,18 +44,20 @@ app.post("/api/posts", (req, res, next) => {
 app.get('/api/posts', (req, res, next) => {
   // res.send('Hello from express');
   // s02FazbMz5V9AERVpo
-  const posts = [
-    {
-      id: "3o23jfa",
-      title: "First server-side post",
-      content: "this is coming from the server"
-    },
-    {
-      id: "dod23jfadadfa",
-      title: "Second server-side post",
-      content: "this is coming from the server!"
-    }
-  ];
+  // const posts = [
+  //   {
+  //     id: "3o23jfa",
+  //     title: "First server-side post",
+  //     content: "this is coming from the server"
+  //   },
+  //   {
+  //     id: "dod23jfadadfa",
+  //     title: "Second server-side post",
+  //     content: "this is coming from the server!"
+  //   }
+  // ];
+  //want to fetch data from posts collection
+  Post.find();
   res.status(200).json({
     message: "posts fetched successfully!",
     posts: posts
--- backend/models/post.js
+++ backend/models/post.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // schema holds custom configuration
 const postSchema = mongoose.Schema({
   title: { type: String, required: true },
-  content: { type: Sting, required: true }
+  content: { type: String, required: true }
 });
 
 module.exports = mongoose.model("Post", postSchema);
